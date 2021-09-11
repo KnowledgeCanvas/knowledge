45ce60580e06addbc33bdf74aa0e245ec48e4423
Rob Royce <robertris2@gmail.com>
2021-09-10 18:59:50 -0700

    Implemented subscription life-cycle management to avoid memory leakage

--- src/kc_angular/projects/main/src/app/projects/project-topic-list/project-topic-list.component.ts
+++ src/kc_angular/projects/main/src/app/projects/project-topic-list/project-topic-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {TopicService} from "../../../../../ks-lib/src/lib/services/topics/topic.service";
 import {MatChipInputEvent} from "@angular/material/chips";
@@ -6,11 +6,10 @@ import {ProjectModel, ProjectUpdateRequest} from "projects/ks-lib/src/lib/models
 import {ProjectService} from "../../../../../ks-lib/src/lib/services/projects/project.service";
 import {MatAutocompleteSelectedEvent, MatAutocompleteTrigger} from "@angular/material/autocomplete";
 import {FormControl} from "@angular/forms";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {map, startWith} from "rxjs/operators";
 import {TopicModel} from "projects/ks-lib/src/lib/models/topic.model";
 import {KsQueueService} from "../../knowledge-source/ks-queue-service/ks-queue.service";
-import {SearchService} from "../../../../../ks-lib/src/lib/services/search/search.service";
 import {BrowserViewDialogService} from "../../../../../ks-lib/src/lib/services/browser-view-dialog/browser-view-dialog.service";
 import {SettingsService} from "../../../../../ks-lib/src/lib/services/settings/settings.service";
 import {KsFactoryService} from "../../../../../ks-lib/src/lib/services/ks-factory/ks-factory.service";
@@ -21,25 +20,20 @@ import {MatDialog} from "@angular/material/dialog";
   templateUrl: './project-topic-list.component.html',
   styleUrls: ['./project-topic-list.component.scss']
 })
-export class ProjectTopicListComponent implements OnInit, OnChanges {
+export class ProjectTopicListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() sourceRef: string | undefined = undefined;
-
   @Output() topicEvent = new EventEmitter<string[]>();
-
   @ViewChild('topicInput') topicInput: ElementRef<HTMLInputElement> = {} as ElementRef;
-
   @ViewChild(MatAutocompleteTrigger, {static: true}) trigger: MatAutocompleteTrigger | undefined;
-
   project: ProjectModel = new ProjectModel('', {value: ''}, 'default');
-
   selectable: boolean = false;
   removable: boolean = true;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
-
   topicCtrl = new FormControl();
   filteredTopics: Observable<string[]>;
   topics: string[] = [];
   allTopics: string[] = ['Computer Science', 'AI/ML'];
+  private subscription: Subscription;
 
   constructor(private topicService: TopicService,
               private browserViewDialogService: BrowserViewDialogService,
@@ -47,9 +41,8 @@ export class ProjectTopicListComponent implements OnInit, OnChanges {
               private searchService: KsQueueService,
               private ksFactory: KsFactoryService,
               private dialog: MatDialog,
-              private projectService: ProjectService,
-              private ksQueueService: KsQueueService) {
-    this.projectService.currentProject.subscribe((project) => {
+              private projectService: ProjectService) {
+    this.subscription = this.projectService.currentProject.subscribe((project) => {
       this.project = project;
       this.topics = project.topics ? project.topics : [];
     });
@@ -72,6 +65,10 @@ export class ProjectTopicListComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   add(event: MatChipInputEvent): void {
@@ -123,7 +120,7 @@ export class ProjectTopicListComponent implements OnInit, OnChanges {
 
   topicClicked(topic: string) {
     let ks = this.ksFactory.searchKS(topic);
-    let dialogRef = this.browserViewDialogService.open({ks: ks});
+    this.browserViewDialogService.open({ks: ks});
   }
 
   onFocus() {
