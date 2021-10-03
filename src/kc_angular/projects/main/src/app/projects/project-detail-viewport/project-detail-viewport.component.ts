d593a34da98d26f5924bb6aed984b2566c55336e
Rob Royce <robertris2@gmail.com>
2021-10-02 17:56:18 -0700

    refactor: implement new KS drag and drop list

--- src/kc_angular/projects/main/src/app/projects/project-detail-viewport/project-detail-viewport.component.ts
+++ src/kc_angular/projects/main/src/app/projects/project-detail-viewport/project-detail-viewport.component.ts
@@ -1,6 +1,10 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {ProjectModel} from "projects/ks-lib/src/lib/models/project.model";
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {ProjectModel, ProjectUpdateRequest} from "projects/ks-lib/src/lib/models/project.model";
 import {ProjectService} from "../../../../../ks-lib/src/lib/services/projects/project.service";
+import {KnowledgeSource} from "../../../../../ks-lib/src/lib/models/knowledge.source.model";
+import {KsQueueService} from "../../knowledge-source/ks-queue-service/ks-queue.service";
+import {Subscription} from "rxjs";
+import {KcDialogRequest, KcDialogService} from "../../../../../ks-lib/src/lib/services/dialog/kc-dialog.service";
 
 
 @Component({
@@ -9,20 +13,114 @@ import {ProjectService} from "../../../../../ks-lib/src/lib/services/projects/pr
   styleUrls: ['./project-detail-viewport.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ProjectDetailViewportComponent implements OnInit {
-  project: ProjectModel | null = null;
+export class ProjectDetailViewportComponent implements OnInit, OnDestroy {
+  kcProject: ProjectModel | null = null;
 
-  constructor(private projectService: ProjectService) {
-    this.project = null;
+  ksQueue: KnowledgeSource[] = [];
 
-    this.projectService.currentProject.subscribe(project => {
+  loading: boolean = false;
+
+  private ksQueueSubscription: Subscription;
+
+  private ksQueueLoadingSubscription: Subscription;
+
+  private kcProjectSubscription: Subscription;
+
+  constructor(private projectService: ProjectService,
+              private ksQueueService: KsQueueService,
+              private confirmDialog: KcDialogService
+  ) {
+    this.ksQueueSubscription = ksQueueService.ksQueue.subscribe((ksQueue) => {
+      this.ksQueue = ksQueue;
+    })
+
+    this.ksQueueLoadingSubscription = ksQueueService.loading.subscribe((loading) => {
+      this.loading = loading;
+    })
+
+    this.kcProjectSubscription = this.projectService.currentProject.subscribe(project => {
+      console.debug('ProjectDetailViewport got updated project: ', project);
       if (project.id.value.trim() !== '')
-        this.project = project;
+        this.kcProject = project;
       else
-        this.project = null;
+        this.kcProject = null;
     });
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.ksQueueSubscription.unsubscribe();
+    this.ksQueueLoadingSubscription.unsubscribe();
+    this.kcProjectSubscription.unsubscribe();
+  }
+
+  ksImported($event: KnowledgeSource[]) {
+    if (!this.kcProject) {
+      return;
+    }
+
+    let update: ProjectUpdateRequest = {
+      addKnowledgeSource: $event,
+      id: this.kcProject.id
+    }
+    console.log('Update Project from ksImported in ProjectDetailViewport...');
+    this.projectService.updateProject(update);
+  }
+
+  ksQueueCleared() {
+    this.ksQueueService.clearResults();
+  }
+
+  ksRemoved($event: KnowledgeSource) {
+    if (!this.kcProject) {
+      return;
+    }
+    let dialogReq: KcDialogRequest = {
+      actionButtonText: "Remove Source",
+      actionToTake: 'delete',
+      cancelButtonText: "Cancel",
+      listToDisplay: [$event],
+      message: "Are you sure you want to remove this knowledge source?",
+      title: "Delete Source"
+    }
+    this.confirmDialog.open(dialogReq);
+    this.confirmDialog.confirmed().subscribe((confirmed) => {
+      if (!this.kcProject || !confirmed) {
+        return;
+      }
+      const update: ProjectUpdateRequest = {
+        id: this.kcProject.id,
+        removeKnowledgeSource: [$event]
+      }
+      console.log('Update Project from ksRemoved in ProjectDetailViewport...');
+      this.projectService.updateProject(update);
+    });
+  }
+
+  ksQueueRemove($event: KnowledgeSource) {
+    this.ksQueueService.remove($event);
+  }
+
+  ksAdded($event: KnowledgeSource[]) {
+    console.log('Adding KS to project: ', $event);
+
+    if (!this.kcProject) {
+      return;
+    }
+
+    let update: ProjectUpdateRequest = {
+      id: this.kcProject.id
+    }
+    console.log('Update Project from ksAdded in ProjectDetailViewport...');
+    this.projectService.updateProject(update);
+  }
+
+  projectChanged($event: ProjectModel) {
+    console.log('Updating project from project details viewport...', $event);
+    this.projectService.updateProject({
+      id: $event.id
+    })
   }
 }
