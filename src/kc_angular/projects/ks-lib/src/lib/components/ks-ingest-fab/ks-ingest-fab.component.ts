59c71edd1ba3dfcfea307c701f714ae0effc8584
Rob Royce <robertris2@gmail.com>
2021-10-04 20:07:20 -0700

    feat(KS FAB): show actions on FAB hover. add animation triggers.

--- src/kc_angular/projects/ks-lib/src/lib/components/ks-ingest-fab/ks-ingest-fab.component.ts
+++ src/kc_angular/projects/ks-lib/src/lib/components/ks-ingest-fab/ks-ingest-fab.component.ts
@@ -8,29 +8,66 @@ import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@ang
 export class KsIngestFabComponent implements OnInit {
   @Input() kcProjectId?: string;
 
-  @Output() fabExpanded = new EventEmitter<boolean>();
+  @Input() ksFabAnimate: boolean = true;
 
-  @Output() fabHovering = new EventEmitter<boolean>();
+  @Output() ksFabExpanded = new EventEmitter<boolean>();
+
+  @Output() ksFabHovering = new EventEmitter<boolean>();
 
   hovering: boolean = false;
+  actions: { label: string, click: () => void }[] = [
+    {
+      label: 'Add a file',
+      click: this.addFile
+    },
+    {
+      label: 'Add a link',
+      click: this.addLink
+    },
+    {
+      label: 'Search all topics',
+      click: this.topicSearch
+    },
+    {
+      label: 'Search the web',
+      click: this.search
+    }
+  ];
+
+  constructor() {
+  }
+
+  addFile() {
+    console.log('Add a file...');
+  }
+
+  addLink() {
+    console.log('Add a link...');
+  }
+
+  topicSearch() {
+    console.log('Topic search...');
+  }
+
+  search() {
+    console.log('Search...');
+  }
 
   @HostListener('mouseenter') onMouseEnter() {
     this.hovering = true;
-    this.fabHovering.emit(this.hovering);
+    this.ksFabHovering.emit(this.hovering);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.hovering = false;
-    this.fabHovering.emit(this.hovering);
+    this.ksFabHovering.emit(this.hovering);
   }
-
-  constructor() { }
 
   ngOnInit(): void {
   }
 
   fabExpand($event: MouseEvent) {
-    this.fabExpanded.emit(true);
+    this.ksFabExpanded.emit(true);
   }
 
   addKs() {
