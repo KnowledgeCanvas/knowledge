5bddc4357c1995338273927530106dfd09a43672
Rob Royce <robertris2@gmail.com>
2021-10-08 12:39:30 -0700

    refactor: remove unused methods, variables, etc from ks context menu component

--- src/kc_angular/projects/ks-lib/src/lib/components/ks-context-menu/ks-context-menu.component.ts
+++ src/kc_angular/projects/ks-lib/src/lib/components/ks-context-menu/ks-context-menu.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {KnowledgeSource} from "../../models/knowledge.source.model";
 import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
 
@@ -13,37 +13,24 @@ export interface ContextMenuModel {
   templateUrl: './ks-context-menu.component.html',
   styleUrls: ['./ks-context-menu.component.scss']
 })
-export class KsContextMenuComponent implements OnInit, AfterViewInit {
+export class KsContextMenuComponent implements OnInit {
   @Input() contextMenuItems!: Array<ContextMenuModel>;
-
   @Input() ks?: KnowledgeSource;
-
   @Output() ksMenuPreviewClicked = new EventEmitter<KnowledgeSource>();
-
   @Output() ksMenuOpenClicked = new EventEmitter<KnowledgeSource>();
-
   @Output() ksMenuShowFileClicked = new EventEmitter<KnowledgeSource>();
-
   @Output() ksMenuCopyLinkClicked = new EventEmitter<KnowledgeSource>();
-
   @Output() ksMenuEditClicked = new EventEmitter<KnowledgeSource>();
-
   @Output() ksMenuRemoveClicked = new EventEmitter<KnowledgeSource>();
-
   @ViewChild(MatMenu) menu!: MatMenu;
-
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger
 
   constructor() {
-
   }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.trigger.openMenu();
     }, 50)
-  }
-
-  ngAfterViewInit() {
   }
 }
