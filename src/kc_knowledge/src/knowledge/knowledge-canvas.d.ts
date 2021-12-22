b0c93a9132195293ba6b2058fc77191bce4d001c
Rob Royce <robertris2@gmail.com>
2021-12-21 21:35:42 -0800

    Add dependency to knowledge project

--- src/kc_knowledge/src/knowledge/knowledge-canvas.d.ts
+++ src/kc_knowledge/src/knowledge/knowledge-canvas.d.ts
@@ -3,14 +3,10 @@ import { EdgeHandlesOptions } from "cytoscape-edgehandles";
 import cxtmenu from 'cytoscape-cxtmenu';
 export declare type KnowledgeSource = any;
 export declare class KnowledgeCanvas {
-    private SAMPLE_DATA;
     private cy;
     private cx;
     private eh;
-    private nav;
     private cyContainer;
-    private nodes;
-    private edges;
     private ksList;
     private cyLayout;
     private cyOptions;
