1e2ab64f04495d7032cea624eabf0e1fdbf07756
Rob Royce <robertris2@gmail.com>
2022-02-01 20:59:48 -0800

    refactor: removed and/or moved tons of files (too many to list).

--- src/kc_angular/projects/ks-lib/src/lib/directives/ksDnd/ks-dnd-vertical.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { KsDndVerticalDirective } from './ks-dnd-vertical.directive';
-
-describe('KsDndVerticalDirective', () => {
-  it('should create an instance', () => {
-    const directive = new KsDndVerticalDirective();
-    expect(directive).toBeTruthy();
-  });
-});
