e6dca1bfc54a7bc0ab76892548d1d0af59f71c6b
Rob Royce <robertris2@gmail.com>
2022-02-02 22:28:22 -0800

    refactor: fix a few errors in SVG files

--- src/kc_electron/src/app/ipc/ipc-knowledge.d.ts
+++ src/kc_electron/src/app/ipc/ipc-knowledge.d.ts
@@ -1,1 +1,9 @@
-export {};
+declare const share: any;
+declare const ipcMain: any;
+declare const app: any;
+declare const path: any;
+declare const BrowserWindow: any;
+declare const KNOWLEDGE_ENTRY: string;
+declare let kcKnowledgeWindow: typeof BrowserWindow | null;
+declare let ipcHandshake: (ksList: []) => Promise<any>;
+declare let getKnowledgeSourceList: any;
