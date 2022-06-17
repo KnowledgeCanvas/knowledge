64b358fd1bb0c78f4a2cc8a7a28af5bb98a50a5d
Rob Royce <robertris2@gmail.com>
2022-06-17 10:38:25 -0700

    feat(Display Settings): Added options to allow user to receive logging messages as "toasts".

--- src/kc_angular/src/app/components/settings-components/display-settings/display-settings.component.ts
+++ src/kc_angular/src/app/components/settings-components/display-settings/display-settings.component.ts
@@ -30,13 +30,32 @@ export class DisplaySettingsComponent implements OnInit {
   displaySettings?: DisplaySettingsModel;
   themes: KcTheme[];
   selectedTheme?: KcTheme;
+  logLevels = [
+    {name: 'Error', value: 'error'},
+    {name: 'Warn', value: 'warn'},
+    {name: 'Debug', value: 'debug'}
+  ];
+  logLevel: string[] = [];
 
   constructor(private ref: DynamicDialogRef, private config: DynamicDialogConfig,
               private themeService: ThemeService, private settingsService: SettingsService,
               private notificationsService: NotificationsService) {
-    this.displaySettings = settingsService.getSettings().display;
+    const displaySettings = settingsService.get().display;
+    this.displaySettings = displaySettings;
     this.themes = themeService.groupedThemes;
     this.selectedTheme = themeService.currentTheme;
+
+    if (displaySettings) {
+      if (displaySettings.logging.debug) {
+        this.logLevel.push('debug');
+      }
+      if (displaySettings.logging.error) {
+        this.logLevel.push('error');
+      }
+      if (displaySettings.logging.warn) {
+        this.logLevel.push('warn');
+      }
+    }
   }
 
   ngOnInit(): void {
@@ -56,14 +75,9 @@ export class DisplaySettingsComponent implements OnInit {
       if (!res) {
         this.selectedTheme = prev;
         this.themeService.switchTheme(prev.code);
-        this.notificationsService.toast({
-          severity: 'warn',
-          summary: 'Oops!',
-          detail: `Sorry, but it seems like the theme "${theme.name}" is unavailable.`,
-          life: 5000
-        });
+        this.notificationsService.warn('DisplaySettings', 'Theme Unavailable', theme.name);
       } else {
-        this.settingsService.saveSettings({display: {theme: this.themeService.currentTheme}});
+        this.settingsService.set({display: {theme: this.themeService.currentTheme}});
       }
     })
   }
@@ -75,8 +89,47 @@ export class DisplaySettingsComponent implements OnInit {
 
     this.themeService.switchTheme(this.themeService.defaultTheme.code).then((res) => {
       console.debug('DisplaySettings: set theme to default: ', res);
-      this.settingsService.saveSettings({display: {theme: this.themeService.currentTheme}});
+      this.settingsService.set({display: {theme: this.themeService.currentTheme}});
     })
     this.selectedTheme = this.themeService.restoreDefault();
+  }
+
+  onLoggingChange($event: any) {
+    let values: string[] = $event.value;
+    if (!values) {
+      return;
+    }
+
+    let logging = {
+      warn: false,
+      debug: false,
+      error: false
+    }
+
+    if (values.includes('warn')) {
+      if (!this.displaySettings?.logging.warn) {
+        this.notificationsService.warn('DisplaySettings', 'Warnings Enabled', 'This is a sample warning message.', 'toast');
+      }
+      logging.warn = true;
+    }
+
+    if (values.includes('error')) {
+      if (!this.displaySettings?.logging.error) {
+        this.notificationsService.error('DisplaySettings', 'Errors Enabled', 'This is a sample error message.', 'toast');
+      }
+      logging.error = true;
+    }
+
+    if (values.includes('debug')) {
+      if (!this.displaySettings?.logging.debug) {
+        this.notificationsService.debug('DisplaySettings', 'Debug Enabled', 'This is a sample debug message.', 'toast');
+      }
+      logging.debug = true;
+    }
+
+    if (this.displaySettings) {
+      this.displaySettings.logging = logging;
+      this.settingsService.set({display: this.displaySettings});
+    }
   }
 }
