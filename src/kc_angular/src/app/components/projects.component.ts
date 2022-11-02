4d87ae686862c03341d898dacc41dd57a2eb8654
Rob Royce <robertris2@gmail.com>
2022-11-01 23:29:42 -0700

    feat(Projects): add graph to Projects page.

--- src/kc_angular/src/app/components/projects.component.ts
+++ src/kc_angular/src/app/components/projects.component.ts
@@ -13,55 +13,71 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ProjectService} from "../services/factory-services/project.service";
 import {KcProject} from "../models/project.model";
 import {Subscription} from "rxjs";
 import {ProjectCommandService} from "../services/command-services/project-command.service";
+import {ContextMenu} from "primeng/contextmenu";
+import {MenuItem} from "primeng/api";
+import {KsCommandService} from "../services/command-services/ks-command.service";
+import {KsContextMenuService} from "../services/factory-services/ks-context-menu.service";
+import {ProjectContextMenuService} from "../services/factory-services/project-context-menu.service";
+import {tap} from "rxjs/operators";
+import {KnowledgeSource} from "../models/knowledge.source.model";
 
 @Component({
   selector: 'app-projects',
   template: `
-    <div class="h-full w-full flex-row-center-start">
-      <app-projects-tree class="h-full max-h-screen flex flex-column flex-shrink-0 border-right-1 border-200"></app-projects-tree>
-      <div class="h-full w-full flex-col-center-center">
-        <div class="h-full w-full flex flex-row surface-0 p-4" style="max-width: min(calc(100vw - 60px), 150rem) !important; max-height: calc(100vh - 100px)">
-          <div class="h-full flex flex-column flex-grow-1 align-items-center justify-content-start overflow-y-auto">
-            <div class="h-full w-full flex flex-row px-2 overflow-y-auto align-content-center justify-content-center align-items-start">
-              <div *ngIf="kcProject" class="h-full w-full flex flex-column">
-                <div>
-                  <app-project-info [project]="kcProject"></app-project-info>
-                </div>
-              </div>
-              <div *ngIf="!kcProject" class="w-full h-full">
-                <p-panel class="h-full w-full">
-                  <div class="h-full w-full text-2xl flex-row-center-center">No Projects</div>
-                  <div class="h-full w-full text-500 flex-row-center-center">
-                    Click the
-                    <button pButton disabled class="p-button-text p-button-plain" icon="pi pi-plus" label="Project"></button>
-                    button to create a Project
-                  </div>
-                </p-panel>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="w-full h-full flex app-splitter-container surface-section">
+      <div class="app-splitter-left">
+        <app-projects-tree class="h-full max-h-screen flex flex-column flex-shrink-0 border-right-1 border-200"></app-projects-tree>
+      </div>
+
+      <div class="app-splitter-divider"></div>
+
+      <div class="app-splitter-right">
+        <graph-canvas #graph
+                      [data]="data"
+                      (onSourceTap)="onSourceTap($event)"
+                      (onSourceCtxtap)="onSourceCtxtap($event)"
+                      (onProjectTap)="onProjectTap($event)"
+                      (onProjectCtxtap)="onProjectCtxtap($event)"
+                      class="w-full h-full">
+        </graph-canvas>
       </div>
     </div>
+
+    <p-contextMenu #cm
+                   styleClass="shadow-7"
+                   [model]="menuItems"
+                   [baseZIndex]="999999"
+                   [autoZIndex]="true"
+                   appendTo="body">
+    </p-contextMenu>
   `,
   styles: []
 })
 export class ProjectsComponent implements OnInit, OnDestroy {
+  @ViewChild('cm') cm!: ContextMenu;
+
   projectId: string = '';
 
   kcProject: KcProject | null = null;
 
   routeSub: Subscription;
 
+  data: any[] = [];
+
+  menuItems: MenuItem[] = [];
+
   constructor(private route: ActivatedRoute,
-              private pCommand: ProjectCommandService,
-              private projects: ProjectService) {
+              private projects: ProjectService,
+              private command: KsCommandService,
+              private context: KsContextMenuService,
+              private pContext: ProjectContextMenuService,
+              private pCommand: ProjectCommandService) {
 
     this.routeSub = route.params.subscribe((params) => {
       this.projectId = params.projectId ?? '';
@@ -71,7 +87,12 @@ export class ProjectsComponent implements OnInit, OnDestroy {
       if (this.projectId !== projects.getCurrentProjectId()?.value) {
         this.projects.setCurrentProject(this.projectId);
       }
+
+      this.data = [];
+      this.build();
     });
+
+    this.projects.currentProject.pipe(tap(this.build)).subscribe()
   }
 
   ngOnInit(): void {
@@ -79,5 +100,84 @@ export class ProjectsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.routeSub.unsubscribe();
+  }
+
+  build() {
+    if (!this.projectId) {
+      return;
+    }
+
+    const projectTree = this.getTree(this.projectId);
+    let data: any[] = [];
+
+    for (let project of projectTree) {
+      data.push({
+        group: 'nodes',
+        data: {
+          id: project.id.value,
+          label: project.name,
+          type: project.id.value === this.projectId ? 'root' : 'project',
+          project: project
+        }
+      });
+
+      for (let sub of project.subprojects) {
+        let edge = {
+          group: 'edges',
+          data: {
+            id: `${project.id.value}-${sub}`,
+            source: project.id.value,
+            target: sub
+          }
+        }
+        data.push(edge);
+      }
+    }
+
+    this.data = data;
+  }
+
+  private getTree(project: string | any): KcProject[] {
+    if (!project) {
+      return [];
+    }
+
+    if (typeof project === 'string') {
+      let p = this.projects.getProject(project);
+      if (!p) {
+        return [];
+      } else {
+        project = p;
+      }
+    }
+
+    let tree = [project];
+    if (!project.subprojects) {
+      return tree;
+    }
+    for (let subProject of project.subprojects) {
+      tree = tree.concat(this.getTree(subProject));
+    }
+
+    return tree;
+  }
+
+  onSourceTap($event: { data: KnowledgeSource; event: MouseEvent }) {
+    this.command.detail($event.data);
+  }
+
+  onSourceCtxtap($event: { data: KnowledgeSource; event: MouseEvent }) {
+    this.menuItems = this.context.generate($event.data)
+    this.cm.show($event.event);
+  }
+
+  onProjectCtxtap($event: { data: KcProject; event: MouseEvent }) {
+    this.menuItems = this.pContext.generate($event.data);
+    this.cm.show($event.event);
+  }
+
+  onProjectTap($event: { data: KcProject; event: MouseEvent }) {
+    console.log('project tap: ', $event);
+    this.pCommand.detail($event.data);
   }
 }
