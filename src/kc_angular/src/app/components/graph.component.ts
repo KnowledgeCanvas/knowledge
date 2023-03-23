d126ae0421e6174eac216b23260dd79144f85b74
Rob Royce <robertris2@gmail.com>
2023-03-22 17:54:11 -0700

    fix: modifying nodes in the graph now updates them appropriately.

--- src/kc_angular/src/app/components/graph.component.ts
+++ src/kc_angular/src/app/components/graph.component.ts
@@ -1,17 +1,17 @@
-/**
- Copyright 2022 Rob Royce
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright (c) 2023 Rob Royce
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
@@ -113,17 +113,8 @@ export class GraphComponent implements OnInit, OnDestroy {
         if (project) {
           this.projectId = project.id.value;
         }
-
-        if (!this.activeProject && project) {
-          this.onBuild.next({})
-        } else if (this.activeProject && project) {
-          if (this.activeProject.id.value !== project.id.value
-            || this.activeProject.knowledgeSource.length !== project.knowledgeSource.length
-            || this.activeProject.subprojects.length !== project.subprojects.length) {
-            this.onBuild.next({})
-          }
-        }
         this.activeProject = project ?? undefined;
+        this.onBuild.next({})
       })
     ).subscribe()
 
