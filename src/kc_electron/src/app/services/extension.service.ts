b367e9e529fb3e46ef0a5442b94a497cdd818a01
Rob Royce <robertris2@gmail.com>
2023-03-24 22:29:20 -0700

    fix: extension server now tries using a different port if the default is already in use.

--- src/kc_electron/src/app/services/extension.service.ts
+++ src/kc_electron/src/app/services/extension.service.ts
@@ -1,17 +1,17 @@
-/**
- Copyright 2022 Rob Royce
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright (c) 2023 Rob Royce
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
@@ -87,7 +87,8 @@ class ExtensionServer {
             console.error('ExtensionServer Error: ', e);
 
             if ((e as any).code && (e as any).code === 'EADDRINUSE') {
-                console.debug('Port in use, retrying...');
+                console.debug(`Port in use, retrying with a new port number (${this.__PORT + 1})...`);
+                this.__PORT++;
                 setTimeout(() => {
                     if (!this.__server) {
                         return;
