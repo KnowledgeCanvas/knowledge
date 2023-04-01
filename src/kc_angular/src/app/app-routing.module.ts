5d55b3f2dae4de725ec8d23a2def9cdb8ef64319
Rob Royce <robertris2@gmail.com>
2023-04-01 14:20:13 -0700

    feat(Chat): initial commit for new chat component.

--- src/kc_angular/src/app/app-routing.module.ts
+++ src/kc_angular/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-/**
- Copyright 2022 Rob Royce
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright (c) 2023 Rob Royce
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
@@ -26,6 +26,7 @@ import {CalendarComponent} from "./components/calendar.component";
 import {StorageSettingsComponent} from "./components/settings/storage-settings.component";
 import {GraphComponent} from "./components/graph.component";
 import {GraphSettingsComponent} from "./components/settings/graph-settings.component";
+import {ChatComponent} from "./components/chat.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/app/inbox/undefined', pathMatch: 'full'},
@@ -35,7 +36,7 @@ const routes: Routes = [
       {
         path: 'inbox/:projectId',
         component: HomeComponent,
-        data: {animation: 'Inbox'}
+        data: {animation: 'Inbox'} // See routeAnimations trigger in animation.ts
       },
       {
         path: 'projects/:projectId',
@@ -61,6 +62,11 @@ const routes: Routes = [
         path: 'calendar/:projectId',
         component: CalendarComponent,
         data: {animation: 'Calendar'}
+      },
+      {
+        path: 'chat/:projectId',
+        component: ChatComponent,
+        data: {animation: 'Chat'}
       },
       {
         path: 'display',
