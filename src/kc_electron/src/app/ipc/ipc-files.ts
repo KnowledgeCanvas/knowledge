f9f8bc2577f11e3421866665d42191970e13e447
Rob Royce <robertris2@gmail.com>
2023-04-18 21:56:28 -0700

    chore: lint electron code base.

--- src/kc_electron/src/app/ipc/ipc-files.ts
+++ src/kc_electron/src/app/ipc/ipc-files.ts
@@ -1,22 +1,26 @@
-/**
- Copyright 2022 Rob Royce
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright (c) 2023 Rob Royce
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-import {IpcMessage, PromptForDirectoryRequest, ThumbnailRequest} from "../../../../kc_shared/models/electron.ipc.model";
+import {
+  IpcMessage,
+  PromptForDirectoryRequest,
+  ThumbnailRequest,
+} from "../../../../kc_shared/models/electron.ipc.model";
 
-const fse = require('fs-extra');
-const settingsService = require('../services/settings.service');
+const fse = require("fs-extra");
+const settingsService = require("../services/settings.service");
 
 const share: any = (global as any).share;
 const ipcMain: any = share.ipcMain;
@@ -28,13 +32,12 @@ const nativeImage: any = share.nativeImage;
 const app: any = share.app;
 const fs: any = share.fs;
 
-
 let promptForDirectory,
-    openLocalFile,
-    getFileThumbnail,
-    getFileIcon,
-    showItemInFolder,
-    dragFileFromApp;
+  openLocalFile,
+  getFileThumbnail,
+  getFileIcon,
+  showItemInFolder,
+  dragFileFromApp;
 
 /**
  * @param path: [string] the path to file selected by user
@@ -42,9 +45,12 @@ let promptForDirectory,
  * @callback none
  * @description shows the file located at <path> in native file explorer
  */
-showItemInFolder = ipcMain.on('A2E:FileSystem:ShowFile', (_: any, path: string) => {
+showItemInFolder = ipcMain.on(
+  "A2E:FileSystem:ShowFile",
+  (_: any, path: string) => {
     shell.showItemInFolder(path);
-});
+  }
+);
 
 /**
  * promptForDirectory
@@ -53,29 +59,31 @@ showItemInFolder = ipcMain.on('A2E:FileSystem:ShowFile', (_: any, path: string) 
  * @callback E2A:FileSystem:DirectoryPrompt
  * @description opens a file chooser dialog that allows the user to select a path
  */
-promptForDirectory = ipcMain.on('A2E:FileSystem:DirectoryPrompt', (event: any, request: PromptForDirectoryRequest) => {
+promptForDirectory = ipcMain.on(
+  "A2E:FileSystem:DirectoryPrompt",
+  (event: any, request: PromptForDirectoryRequest) => {
     if (!request) {
-        request = {properties: ['openDirectory', "createDirectory"]}
+      request = { properties: ["openDirectory", "createDirectory"] };
     }
-    let kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
+    const kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
     const result = dialog.showOpenDialogSync(kcMainWindow, request);
-    let response: IpcMessage = {
-        error: undefined,
-        success: undefined
-    }
+    const response: IpcMessage = {
+      error: undefined,
+      success: undefined,
+    };
     if (result && result[0]) {
-        let dir = result[0];
-        response.success = {data: dir}
+      const dir = result[0];
+      response.success = { data: dir };
     } else {
-        response.error = {
-            code: 412,
-            label: http.STATUS_CODES['412'],
-            message: 'Invalid or non-existent path or directory chosen.'
-        }
+      response.error = {
+        code: 412,
+        label: http.STATUS_CODES["412"],
+        message: "Invalid or non-existent path or directory chosen.",
+      };
     }
-    kcMainWindow.webContents.send('E2A:FileSystem:DirectoryPrompt', response);
-})
-
+    kcMainWindow.webContents.send("E2A:FileSystem:DirectoryPrompt", response);
+  }
+);
 
 /**
  * openLocalFile
@@ -84,23 +92,25 @@ promptForDirectory = ipcMain.on('A2E:FileSystem:DirectoryPrompt', (event: any, r
  * @callback E2A:FileSystem:OpenFile [boolean]
  * @description opens the file located at filePath in the OS-level default application
  */
-openLocalFile = ipcMain.on('A2E:FileSystem:OpenFile', (event: any, filePath: string) => {
+openLocalFile = ipcMain.on(
+  "A2E:FileSystem:OpenFile",
+  (event: any, filePath: string) => {
     shell.openPath(path.resolve(filePath)).then((outcome: string) => {
-        let response: IpcMessage = {error: undefined, success: undefined}
-        if (outcome === '') {
-            response.success = {data: true}
-        } else {
-            response.error = {
-                message: outcome,
-                code: 501,
-                label: http.STATUS_CODES['501']
-            }
-        }
-        let kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
-        kcMainWindow.webContents.send('E2A:FileSystem:OpenFile', response);
+      const response: IpcMessage = { error: undefined, success: undefined };
+      if (outcome === "") {
+        response.success = { data: true };
+      } else {
+        response.error = {
+          message: outcome,
+          code: 501,
+          label: http.STATUS_CODES["501"],
+        };
+      }
+      const kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
+      kcMainWindow.webContents.send("E2A:FileSystem:OpenFile", response);
     });
-});
-
+  }
+);
 
 /**
  * getFileThumbnail
@@ -109,64 +119,97 @@ openLocalFile = ipcMain.on('A2E:FileSystem:OpenFile', (event: any, filePath: str
  * @callback E2A:FileSystem:FileThumbnail [IpcResponse[]]
  * @description requests thumbnails for files in each request and returns them in an array
  */
-getFileThumbnail = ipcMain.on('A2E:FileSystem:FileThumbnail', (event: any, requests: ThumbnailRequest[]) => {
-    let kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
+getFileThumbnail = ipcMain.on(
+  "A2E:FileSystem:FileThumbnail",
+  (event: any, requests: ThumbnailRequest[]) => {
+    const kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
     let responses: IpcMessage[] = [];
-    let actions: any[] = [];
+    const actions: any[] = [];
 
     if (requests.length <= 0) {
-        responses = [{
-            error: {code: 412, label: http.STATUS_CODES['412'], message: 'Invalid thumbnail request.'},
-            success: undefined
-        }]
-        let kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
-        kcMainWindow.webContents.send('E2A:FileSystem:FileThumbnail', responses);
-        return;
+      responses = [
+        {
+          error: {
+            code: 412,
+            label: http.STATUS_CODES["412"],
+            message: "Invalid thumbnail request.",
+          },
+          success: undefined,
+        },
+      ];
+      const kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
+      kcMainWindow.webContents.send("E2A:FileSystem:FileThumbnail", responses);
+      return;
     }
 
-    if (process.platform != 'darwin' && process.platform != 'win32') {
-        // The `createThumbnailFromPath` function is only available on Windows and MacOS
-        responses = [{
-            error: {code: 412, label: http.STATUS_CODES['412'], message: 'Thumbnail creation only supported on Windows and MacOS'},
-            success: undefined
-        }]
-        let kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
-        kcMainWindow.webContents.send('E2A:FileSystem:FileThumbnail', responses);
-        return;
+    if (process.platform != "darwin" && process.platform != "win32") {
+      // The `createThumbnailFromPath` function is only available on Windows and MacOS
+      responses = [
+        {
+          error: {
+            code: 412,
+            label: http.STATUS_CODES["412"],
+            message: "Thumbnail creation only supported on Windows and MacOS",
+          },
+          success: undefined,
+        },
+      ];
+      const kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
+      kcMainWindow.webContents.send("E2A:FileSystem:FileThumbnail", responses);
+      return;
     }
 
-    for (let request of requests) {
-        const height = request.height ?? 1920;
-        const width = request.width ?? 1080;
-        actions.push(nativeImage.createThumbnailFromPath(path.resolve(request.path), {
-            width: width,
-            height: height
-        }));
+    for (const request of requests) {
+      const height = request.height ?? 1920;
+      const width = request.width ?? 1080;
+      actions.push(
+        nativeImage.createThumbnailFromPath(path.resolve(request.path), {
+          width: width,
+          height: height,
+        })
+      );
     }
 
-    Promise.all(actions).then((thumbnails) => {
+    Promise.all(actions)
+      .then((thumbnails) => {
         for (let i = 0; i < thumbnails.length; i++) {
-            let response: IpcMessage = {
-                error: undefined,
-                success: {data: {id: requests[i].id ?? '', thumbnail: thumbnails[i].toDataURL()}}
-            }
-            responses.push(response);
-        }
-        kcMainWindow.webContents.send('E2A:FileSystem:FileThumbnail', responses);
-    }).catch((reason) => {
-        console.error('Caught promise exception while getting thumbnail: ', reason, requests);
-        let response: IpcMessage = {
-            error: {
-                code: 501,
-                label: http.STATUS_CODES['501'],
-                message: 'OS failed to generate thumbnails'
+          const response: IpcMessage = {
+            error: undefined,
+            success: {
+              data: {
+                id: requests[i].id ?? "",
+                thumbnail: thumbnails[i].toDataURL(),
+              },
             },
-            success: undefined
+          };
+          responses.push(response);
         }
+        kcMainWindow.webContents.send(
+          "E2A:FileSystem:FileThumbnail",
+          responses
+        );
+      })
+      .catch((reason) => {
+        console.error(
+          "Caught promise exception while getting thumbnail: ",
+          reason,
+          requests
+        );
+        const response: IpcMessage = {
+          error: {
+            code: 501,
+            label: http.STATUS_CODES["501"],
+            message: "OS failed to generate thumbnails",
+          },
+          success: undefined,
+        };
         // The caller is expecting an array, so even though we're only sending a single response, we wrap it in array
-        kcMainWindow.webContents.send('E2A:FileSystem:FileThumbnail', [response]);
-    });
-});
+        kcMainWindow.webContents.send("E2A:FileSystem:FileThumbnail", [
+          response,
+        ]);
+      });
+  }
+);
 
 /**
  * getFileIcon
@@ -175,29 +218,31 @@ getFileThumbnail = ipcMain.on('A2E:FileSystem:FileThumbnail', (event: any, reque
  * @callback E2A:FileSystem:FileIcon [IpcResponse[]]
  * @description requests icons for files in each request and returns them in an array
  */
-getFileIcon = ipcMain.on('A2E:FileSystem:FileIcon', (event: any, filePaths: string[]) => {
+getFileIcon = ipcMain.on(
+  "A2E:FileSystem:FileIcon",
+  (event: any, filePaths: string[]) => {
     if (filePaths.length <= 0) {
-        return;
+      return;
     }
-    let kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
-    let responses: IpcMessage[] = [];
-    let options = {size: 'normal'}
-    let actions: any[] = [];
-    for (let filePath of filePaths) {
-        actions.push(app.getFileIcon(path.resolve(filePath), options));
+    const kcMainWindow: any = share.BrowserWindow.getAllWindows()[0];
+    const responses: IpcMessage[] = [];
+    const options = { size: "normal" };
+    const actions: any[] = [];
+    for (const filePath of filePaths) {
+      actions.push(app.getFileIcon(path.resolve(filePath), options));
     }
     Promise.all(actions).then((icons) => {
-        for (let icon of icons) {
-            let response: IpcMessage = {
-                error: undefined,
-                success: {data: icon.toDataURL()}
-            }
-            responses.push(response);
-        }
-        kcMainWindow.webContents.send('E2A:FileSystem:FileIcon', responses);
+      for (const icon of icons) {
+        const response: IpcMessage = {
+          error: undefined,
+          success: { data: icon.toDataURL() },
+        };
+        responses.push(response);
+      }
+      kcMainWindow.webContents.send("E2A:FileSystem:FileIcon", responses);
     });
-});
-
+  }
+);
 
 /**
  * @param args: [KnowledgeSource] the KS (file) to be exported
@@ -205,57 +250,73 @@ getFileIcon = ipcMain.on('A2E:FileSystem:FileIcon', (event: any, filePaths: stri
  * @callback none
  * @description creates a new drag-and-drop event using the actual file pointed to be the KS
  */
-dragFileFromApp = ipcMain.on('A2E:FileSystem:StartDrag', (event: any, args: any) => {
-    let ks = args;
+dragFileFromApp = ipcMain.on(
+  "A2E:FileSystem:StartDrag",
+  (event: any, args: any) => {
+    const ks = args;
     if (!ks) {
-        console.error('Invalid or no Knowledge Source passed to electron IPC A2E:FileSystem:StartDrag handler.');
-        return;
+      console.error(
+        "Invalid or no Knowledge Source passed to electron IPC A2E:FileSystem:StartDrag handler."
+      );
+      return;
     }
 
-    if (ks.ingestType !== 'file') {
-        console.error('Cannot drag and drop non-file sources just yet...');
-        return;
+    if (ks.ingestType !== "file") {
+      console.error("Cannot drag and drop non-file sources just yet...");
+      return;
     }
 
-    let options = {size: 'normal'}
+    const options = { size: "normal" };
 
     // First, copy file to centralized location
     const appPath = settingsService.getSettings().system.appPath;
-    const copyPath = path.join(appPath, 'tmp');
+    const copyPath = path.join(appPath, "tmp");
     const filename = path.basename(ks.accessLink);
     const filePath = path.join(copyPath, filename);
 
     // TODO: periodically purge the tmp folder
     try {
-        fs.mkdirSync(copyPath, {recursive: true});
+      fs.mkdirSync(copyPath, { recursive: true });
     } catch (e) {
-        console.error('File IPC Unable to create temporary directory for drag/drop...');
+      console.error(
+        "File IPC Unable to create temporary directory for drag/drop..."
+      );
     }
 
     try {
-        fse.cpSync(ks.accessLink, filePath);
+      fse.cpSync(ks.accessLink, filePath);
     } catch (e) {
-        console.error('File IPC Unable to copy file to temporary directory for drag/drop...');
+      console.error(
+        "File IPC Unable to copy file to temporary directory for drag/drop..."
+      );
     }
 
     // Then, use NEW file for drag handler
 
-    app.getFileIcon(filePath, options).then((icon: any) => {
+    app
+      .getFileIcon(filePath, options)
+      .then((icon: any) => {
         event.sender.startDrag({
-            file: path.resolve(filePath),
-            icon: icon
+          file: path.resolve(filePath),
+          icon: icon,
         });
 
         setTimeout(() => {
-            try {
-                fse.remove(filePath);
-            } catch (e) {
-            }
+          try {
+            fse.remove(filePath);
+          } catch (e) {}
         }, 30000);
-    }).catch((reason: any) => {
-        console.error('Error in main process, dragging file from app', reason);
-    });
-});
+      })
+      .catch((reason: any) => {
+        console.error("Error in main process, dragging file from app", reason);
+      });
+  }
+);
 
-
-module.exports = {promptForDirectory, openLocalFile, getFileThumbnail: getFileThumbnail, getFileIcon, dragFileFromApp}
+module.exports = {
+  promptForDirectory,
+  openLocalFile,
+  getFileThumbnail: getFileThumbnail,
+  getFileIcon,
+  dragFileFromApp,
+};
--- src/kc_electron/src/electron.ts
+++ src/kc_electron/src/electron.ts
@@ -1,65 +1,78 @@
-/**
- Copyright 2022 Rob Royce
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright (c) 2023 Rob Royce
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
-import {UpdateCheckResult} from "electron-updater";
+import { UpdateCheckResult } from "electron-updater";
 
-const {app, BrowserWindow, BrowserView, ipcMain, dialog, shell} = require('electron');
-const {autoUpdater} = require("electron-updater");
-const nativeImage = require('electron').nativeImage
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-const settingsService = require('./app/services/settings.service');
-const uuid = require('uuid');
-const MAIN_ENTRY: string = path.join(app.getAppPath(), 'src', 'kc_angular', 'dist', 'main', 'index.html');
+const {
+  app,
+  BrowserWindow,
+  BrowserView,
+  ipcMain,
+  dialog,
+  shell,
+} = require("electron");
+const { autoUpdater } = require("electron-updater");
+const nativeImage = require("electron").nativeImage;
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const settingsService = require("./app/services/settings.service");
+const uuid = require("uuid");
+const MAIN_ENTRY: string = path.join(
+  app.getAppPath(),
+  "src",
+  "kc_angular",
+  "dist",
+  "main",
+  "index.html"
+);
 
 (global as any).share = {
-    BrowserView,
-    BrowserWindow,
-    app,
-    autoUpdater,
-    dialog,
-    fs,
-    http,
-    ipcMain,
-    nativeImage,
-    path,
-    settingsService,
-    shell,
-    url,
-    uuid,
+  BrowserView,
+  BrowserWindow,
+  app,
+  autoUpdater,
+  dialog,
+  fs,
+  http,
+  ipcMain,
+  nativeImage,
+  path,
+  settingsService,
+  shell,
+  url,
+  uuid,
 };
 
-console.log('Dirname: ', __dirname);
+console.log("Dirname: ", __dirname);
 
 // Setup auto update
-require('./app/services/auto.update.service');
+require("./app/services/auto.update.service");
 
 // Setup IPC
-require('./app/ipc');
+require("./app/ipc");
 
 // Setup knowledge source ingestion
-require('./app/services/index');
+require("./app/services/index");
 
-const browserIpc = require('./app/ipc').browserIpc;
-
+const browserIpc = require("./app/ipc").browserIpc;
 
 // Get application settings
-let appEnv = settingsService.getSettings();
+const appEnv = settingsService.getSettings();
 
 // Declare main window for later use
 let kcMainWindow: any;
@@ -68,105 +81,121 @@ let kcMainWindow: any;
  * Main Window Functions
  */
 function createMainWindow() {
-    let WIDTH: number = parseInt(appEnv.env.DEFAULT_WINDOW_WIDTH);
-    let HEIGHT: number = parseInt(appEnv.env.DEFAULT_WINDOW_HEIGHT);
-    let darkMode = appEnv.display.theme.isDark;
-    let backgroundColor = darkMode ? '#1E1E1E' : '#F9F9F9';
-    console.log('Theme: ', appEnv.display.theme);
+  const WIDTH: number = parseInt(appEnv.env.DEFAULT_WINDOW_WIDTH);
+  const HEIGHT: number = parseInt(appEnv.env.DEFAULT_WINDOW_HEIGHT);
+  const darkMode = appEnv.display.theme.isDark;
+  const backgroundColor = darkMode ? "#1E1E1E" : "#F9F9F9";
+  console.log("Theme: ", appEnv.display.theme);
 
-    console.log('Knowledge storage path: ', app.getPath('userData'));
+  console.log("Knowledge storage path: ", app.getPath("userData"));
 
-    app.setName('Knowledge');
+  app.setName("Knowledge");
 
-    // kcMainWindow = new BrowserWindow(config);
-    kcMainWindow = new BrowserWindow({
-        title: 'Knowledge',
-        backgroundColor: backgroundColor,
-        width: WIDTH ? WIDTH : 1280,
-        height: HEIGHT ? HEIGHT : 1600,
-        minWidth: 800,
-        minHeight: 800,
-        frame: false,
-        icon: path.resolve(app.getAppPath(), '..', 'icon.png'),
-        show: false,
-        webPreferences: {
-            nodeIntegration: false, // is default value after Electron v5
-            contextIsolation: true, // protect against prototype pollution
-            preload: path.join(app.getAppPath(), 'src', 'kc_electron', 'dist', 'preload.js')
-        }
-    });
+  // kcMainWindow = new BrowserWindow(config);
+  kcMainWindow = new BrowserWindow({
+    title: "Knowledge",
+    backgroundColor: backgroundColor,
+    width: WIDTH ? WIDTH : 1280,
+    height: HEIGHT ? HEIGHT : 1600,
+    minWidth: 800,
+    minHeight: 800,
+    frame: false,
+    icon: path.resolve(app.getAppPath(), "..", "icon.png"),
+    show: false,
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      preload: path.join(
+        app.getAppPath(),
+        "src",
+        "kc_electron",
+        "dist",
+        "preload.js"
+      ),
+    },
+  });
 
-    setMainWindowListeners();
+  setMainWindowListeners();
 }
 
 function setMainWindowListeners() {
-    kcMainWindow.webContents.on('did-fail-load',
-        (event: any, errorCode: number, errorDescription: string, validatedURL: string, ...other: any) => {
-            event.preventDefault();
+  kcMainWindow.webContents.on(
+    "did-fail-load",
+    (
+      event: any,
+      errorCode: number,
+      errorDescription: string,
+      validatedURL: string,
+      ...other: any
+    ) => {
+      event.preventDefault();
 
-            const defaultAction = () => {
-                browserIpc.destroyBrowserViews(kcMainWindow);
-                kcMainWindow.loadFile(MAIN_ENTRY);
-                return;
-            }
+      const defaultAction = () => {
+        browserIpc.destroyBrowserViews(kcMainWindow);
+        kcMainWindow.loadFile(MAIN_ENTRY);
+        return;
+      };
 
-            const sender = event.sender;
+      const sender = event.sender;
 
-            if (errorCode === -6 || errorDescription === 'ERR_FILE_NOT_FOUND') {
-                if (validatedURL.includes(process.cwd())) {
-                    defaultAction();
-                }
-            }
+      if (errorCode === -6 || errorDescription === "ERR_FILE_NOT_FOUND") {
+        if (validatedURL.includes(process.cwd())) {
+          defaultAction();
+        }
+      }
 
-            if (!sender || sender.isCrashed() || sender.isDestroyed()) {
-                defaultAction();
-            }
-        })
+      if (!sender || sender.isCrashed() || sender.isDestroyed()) {
+        defaultAction();
+      }
+    }
+  );
 
-    // Destroy window on close
-    kcMainWindow.on('closed', function () {
-        kcMainWindow = null;
-        app.quit();
-    });
+  // Destroy window on close
+  kcMainWindow.on("closed", function () {
+    kcMainWindow = null;
+    app.quit();
+  });
 
-    // Handle event in which a new window is created (i.e. when a user clicks on a link that is meant to open in new tab, etc)
-    kcMainWindow.webContents.on('new-window', (event: any, url: string) => {
-        console.log('New window requested: ', url);
-        event.preventDefault();
-        shell.openExternal(url);
-    });
+  // Handle event in which a new window is created (i.e. when a user clicks on a link that is meant to open in new tab, etc)
+  kcMainWindow.webContents.on("new-window", (event: any, url: string) => {
+    console.log("New window requested: ", url);
+    event.preventDefault();
+    shell.openExternal(url);
+  });
 
-    // Show the window once it's ready
-    kcMainWindow.once('ready-to-show', () => {
-        kcMainWindow.show();
-    });
+  // Show the window once it's ready
+  kcMainWindow.once("ready-to-show", () => {
+    kcMainWindow.show();
+  });
 }
 
-app.on('window-all-closed', function () {
-    // MacOS apps typically do not quit all the way when a window is closed...
-    if (process.platform !== 'darwin')
-        app.quit()
+app.on("window-all-closed", function () {
+  // MacOS apps typically do not quit all the way when a window is closed...
+  if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow();
-    }
-});
-
-
-app.on('ready', function () {
-    // Create window but wait to load and show until after update
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createMainWindow();
+  }
+});
 
-    autoUpdater.checkForUpdatesAndNotify().then((update: UpdateCheckResult | null) => {
-        if (update) {
-            // TODO: take action on new versions, such as an "Updating" window or compatibility checks
-            console.log('Update Check Results: ', update);
-        }
-    }).catch((reason: any) => {
-        console.error('Update Check Error: ', reason);
-    }).finally(() => {
-        kcMainWindow.loadFile(MAIN_ENTRY);
+app.on("ready", function () {
+  // Create window but wait to load and show until after update
+  createMainWindow();
+
+  autoUpdater
+    .checkForUpdatesAndNotify()
+    .then((update: UpdateCheckResult | null) => {
+      if (update) {
+        // TODO: take action on new versions, such as an "Updating" window or compatibility checks
+        console.log("Update Check Results: ", update);
+      }
     })
+    .catch((reason: any) => {
+      console.error("Update Check Error: ", reason);
+    })
+    .finally(() => {
+      kcMainWindow.loadFile(MAIN_ENTRY);
+    });
 });
