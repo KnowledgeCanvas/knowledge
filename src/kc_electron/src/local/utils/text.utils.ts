f10907b96189884aaed3825a3154e0efd7a3f79b
Rob Royce <robertris2@gmail.com>
2023-05-10 20:56:51 -0700

    feature: add right-click actions to browser view.

--- src/kc_electron/src/local/utils/text.utils.ts
+++ src/kc_electron/src/local/utils/text.utils.ts
@@ -13,3 +13,48 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+export class TextUtils {
+  constructor() {
+    throw new Error("TextUtils is a static class and cannot be instantiated");
+  }
+
+  static isNullOrWhitespace(text: string): boolean {
+    return text === null || text.match(/^ *$/) !== null;
+  }
+
+  static isNullOrEmpty(text: string): boolean {
+    return text === null || text === "";
+  }
+
+  static isNullOrWhitespaceOrEmpty(text: string): boolean {
+    return TextUtils.isNullOrWhitespace(text) || TextUtils.isNullOrEmpty(text);
+  }
+
+  static convertToAscii(text: string): string {
+    return text.replace(/[^\x00-\x7F]/g, "");
+  }
+
+  static convertToBase64(text: string): string {
+    return Buffer.from(text).toString("base64");
+  }
+
+  static convertFromBase64(text: string): string {
+    return Buffer.from(text, "base64").toString();
+  }
+
+  static convertToBase64Url(text: string): string {
+    return TextUtils.convertToBase64(text)
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=/g, "");
+  }
+
+  static chunk(text: string, size: number): string[] {
+    const chunks = [];
+    for (let i = 0; i < text.length; i += size) {
+      chunks.push(text.substring(i, i + size - 1));
+    }
+    return chunks;
+  }
+}
