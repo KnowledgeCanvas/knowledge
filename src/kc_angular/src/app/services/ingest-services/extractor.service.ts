f08abeb26de9dd397069d7778fe24d27dee6ef2e
Rob Royce <robertris2@gmail.com>
2024-01-21 11:21:17 -0800

    feat: add "citation" metadata tag to extractor.

--- src/kc_angular/src/app/services/ingest-services/extractor.service.ts
+++ src/kc_angular/src/app/services/ingest-services/extractor.service.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Rob Royce
+ * Copyright (c) 2023-2024 Rob Royce
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -216,6 +216,7 @@ export class ExtractorService {
                       )) /* Twitter (but not app) */ ||
                     n.startsWith('description') /* Description */ ||
                     n.startsWith('article:') /* Articles */ ||
+                    n.startsWith('citation') /* Citation */ ||
                     n.startsWith('keywords')) /* Keywords */
               );
 
